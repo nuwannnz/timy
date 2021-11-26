06f23812ca524c677ccac1733fd92ec01dd1bca4
Nuwan Karunarathna <nuwankarunarathna504@gmail.com>
2021-11-26 10:41:45 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd';
+import React from 'react';
+import './App.css';
+
+const App:React.FC = () => {
+  return (
+    <div className="App">
+      <Button type="primary">Hello</Button>
+    </div>
+  );
+}
+
+export default App;
